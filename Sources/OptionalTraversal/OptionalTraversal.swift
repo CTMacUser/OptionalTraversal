806631af06ac26587280cc2683f564fde17b9e37
Daryle Walker <dwalker07@yahoo.com>
2019-06-28 01:59:45 -0400

    Add index operations that return Optional
    Add property that returns Collection.startIndex, except it returns NIL on endIndex instead.  Add variants of the index iteration methods that return NIL instead of endIndex when needed.  Add corresponding tests.

--- Sources/OptionalTraversal/OptionalTraversal.swift
+++ Sources/OptionalTraversal/OptionalTraversal.swift
@@ -1,3 +1,78 @@
-struct OptionalTraversal {
-    var text = "Hello, World!"
+//
+//  OptionalTraversal.swift
+//  OptionalTraversal
+//
+//  Created by Daryle Walker on 6/26/19.
+//
+
+
+extension Collection {
+
+    /// The position of the first element, if any.
+    ///
+    /// If the collection is empty, expresses `nil`.  So checking the status of
+    /// this property always ensures an element.
+    public var startingIndex: Index? {
+        let result = startIndex
+        return result != endIndex ? result : nil
+    }
+
+    /// Returns the position immediately after the given index, if said index
+    /// references a valid subscript argument.
+    ///
+    /// If `nil` is returned, then `i` was already the last subscriptable index.
+    ///
+    /// - Parameter i: A valid index of the collection.
+    /// - Returns: The index value immediately after `i` if it isn't `endIndex`.
+    ///   Otherwise, `nil`.
+    public func elementIndex(after i: Index) -> Index? {
+        let end = endIndex
+        guard i < end else { return nil }
+
+        let next = index(after: i)
+        return next != end ? next : nil
+    }
+
+    /// Returns an index that is the specified distance from the given index, if
+    /// said index references a valid subscript argument.
+    ///
+    /// If `nil` is returned, then valid elements stop happening before that
+    /// distance (including direction) from `i`.
+    ///
+    /// - Parameter i: A valid index of the collection.
+    /// - Parameter distance: The distance to offset `i`.  It must not be
+    ///   negative unless the collection conforms to `BidirectionalCollection`.
+    /// - Returns: An index offset by `distance` from the index `i`, if it stays
+    ///   within `indices`.  Otherwise, `nil`.
+    /// - Complexity: O(1) if the collection conforms to `RandomAccessCollection`,
+    ///   O(*k*) otherwise, where *k* is the absolute value of `distance`.
+    public func elementIndex(_ i: Index, offsetBy distance: Int) -> Index? {
+        if distance >= 0 {
+            let end = endIndex
+            let result = index(i, offsetBy: distance, limitedBy: end)
+            return result != end ? result : nil
+        } else {
+            return index(i, offsetBy: distance, limitedBy: startIndex)
+        }
+    }
+
+}
+
+extension BidirectionalCollection {
+
+    /// Returns the position immediately before the given index, if said index
+    /// references a valid subscript argument.
+    ///
+    /// If `nil` is returned, then `i` was already the first subscriptable
+    /// index.
+    ///
+    /// - Parameter i: A valid index of the collection.
+    /// - Returns: The index value immediately before `i` if it wouldn't go past
+    ///   `startIndex`.  Otherwise, `nil`.
+    public func elementIndex(before i: Index) -> Index? {
+        guard i > startIndex else { return nil }
+
+        return index(before: i)
+    }
+
 }
--- Tests/OptionalTraversalTests/OptionalTraversalTests.swift
+++ Tests/OptionalTraversalTests/OptionalTraversalTests.swift
@@ -1,15 +1,107 @@
+//
+//  OptionalTraversalTests.swift
+//  OptionalTraversal
+//
+//  Created by Daryle Walker on 6/26/19.
+//
+
 import XCTest
 @testable import OptionalTraversal
 
+
 final class OptionalTraversalTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(OptionalTraversal().text, "Hello, World!")
+
+    // Test the starting-index property
+    func testStartingIndex() {
+        let test1 = EmptyCollection<Int>()
+        XCTAssertNil(test1.startingIndex)
+
+        let test2 = CollectionOfOne(1)
+        XCTAssertNotNil(test2.startingIndex)
+        XCTAssertEqual(test2.startingIndex, test2.startIndex)
+        XCTAssertNotEqual(test2.startingIndex, test2.endIndex)
+
+        let test3 = [Int](), test4 = [1, 2, 3, 4]
+        XCTAssertNil(test3.startingIndex)
+        XCTAssertNotNil(test4.startingIndex)
+        XCTAssertEqual(test4.startingIndex, test4.startIndex)
+        XCTAssertNotEqual(test4.startingIndex, test4.endIndex)
+    }
+
+    // Test nil-testing of other indices
+    func testElementIndex() {
+        let array = Array(0..<5), indices = Array(array.indices)
+        XCTAssertEqual(array.startingIndex, indices.startingIndex)
+
+        // elementIndex(after:)
+        XCTAssertEqual(array.elementIndex(after: indices[0]), indices[1])
+        XCTAssertEqual(array.elementIndex(after: indices[1]), indices[2])
+        XCTAssertEqual(array.elementIndex(after: indices[2]), indices[3])
+        XCTAssertEqual(array.elementIndex(after: indices[3]), indices[4])
+        XCTAssertNil(array.elementIndex(after: indices[4]))
+        XCTAssertNil(array.elementIndex(after: array.endIndex))
+
+        // elementIndex(before:)
+        XCTAssertEqual(array.elementIndex(before: array.endIndex), indices[4])
+        XCTAssertEqual(array.elementIndex(before: indices[4]), indices[3])
+        XCTAssertEqual(array.elementIndex(before: indices[3]), indices[2])
+        XCTAssertEqual(array.elementIndex(before: indices[2]), indices[1])
+        XCTAssertEqual(array.elementIndex(before: indices[1]), indices[0])
+        XCTAssertNil(array.elementIndex(before: indices[0]))
+
+        // elementIndex(_: offsetBy:), all combinations
+        XCTAssertNil(array.elementIndex(indices[0], offsetBy: -1))
+        XCTAssertEqual(array.elementIndex(indices[0], offsetBy: 0), indices[0])
+        XCTAssertEqual(array.elementIndex(indices[0], offsetBy: 1), indices[1])
+        XCTAssertEqual(array.elementIndex(indices[0], offsetBy: 2), indices[2])
+        XCTAssertEqual(array.elementIndex(indices[0], offsetBy: 3), indices[3])
+        XCTAssertEqual(array.elementIndex(indices[0], offsetBy: 4), indices[4])
+        XCTAssertNil(array.elementIndex(indices[0], offsetBy: 5))
+
+        XCTAssertNil(array.elementIndex(indices[1], offsetBy: -2))
+        XCTAssertEqual(array.elementIndex(indices[1], offsetBy: -1), indices[0])
+        XCTAssertEqual(array.elementIndex(indices[1], offsetBy: 0), indices[1])
+        XCTAssertEqual(array.elementIndex(indices[1], offsetBy: 1), indices[2])
+        XCTAssertEqual(array.elementIndex(indices[1], offsetBy: 2), indices[3])
+        XCTAssertEqual(array.elementIndex(indices[1], offsetBy: 3), indices[4])
+        XCTAssertNil(array.elementIndex(indices[1], offsetBy: 4))
+
+        XCTAssertNil(array.elementIndex(indices[2], offsetBy: -3))
+        XCTAssertEqual(array.elementIndex(indices[2], offsetBy: -2), indices[0])
+        XCTAssertEqual(array.elementIndex(indices[2], offsetBy: -1), indices[1])
+        XCTAssertEqual(array.elementIndex(indices[2], offsetBy: 0), indices[2])
+        XCTAssertEqual(array.elementIndex(indices[2], offsetBy: 1), indices[3])
+        XCTAssertEqual(array.elementIndex(indices[2], offsetBy: 2), indices[4])
+        XCTAssertNil(array.elementIndex(indices[2], offsetBy: 3))
+
+        XCTAssertNil(array.elementIndex(indices[3], offsetBy: -4))
+        XCTAssertEqual(array.elementIndex(indices[3], offsetBy: -3), indices[0])
+        XCTAssertEqual(array.elementIndex(indices[3], offsetBy: -2), indices[1])
+        XCTAssertEqual(array.elementIndex(indices[3], offsetBy: -1), indices[2])
+        XCTAssertEqual(array.elementIndex(indices[3], offsetBy: 0), indices[3])
+        XCTAssertEqual(array.elementIndex(indices[3], offsetBy: 1), indices[4])
+        XCTAssertNil(array.elementIndex(indices[3], offsetBy: 2))
+
+        XCTAssertNil(array.elementIndex(indices[4], offsetBy: -5))
+        XCTAssertEqual(array.elementIndex(indices[4], offsetBy: -4), indices[0])
+        XCTAssertEqual(array.elementIndex(indices[4], offsetBy: -3), indices[1])
+        XCTAssertEqual(array.elementIndex(indices[4], offsetBy: -2), indices[2])
+        XCTAssertEqual(array.elementIndex(indices[4], offsetBy: -1), indices[3])
+        XCTAssertEqual(array.elementIndex(indices[4], offsetBy: 0), indices[4])
+        XCTAssertNil(array.elementIndex(indices[4], offsetBy: 1))
+
+        XCTAssertNil(array.elementIndex(array.endIndex, offsetBy: -6))
+        XCTAssertEqual(array.elementIndex(array.endIndex, offsetBy: -5), indices[0])
+        XCTAssertEqual(array.elementIndex(array.endIndex, offsetBy: -4), indices[1])
+        XCTAssertEqual(array.elementIndex(array.endIndex, offsetBy: -3), indices[2])
+        XCTAssertEqual(array.elementIndex(array.endIndex, offsetBy: -2), indices[3])
+        XCTAssertEqual(array.elementIndex(array.endIndex, offsetBy: -1), indices[4])
+        XCTAssertNil(array.elementIndex(array.endIndex, offsetBy: 0))
     }
 
     static var allTests = [
-        ("testExample", testExample),
+        ("testStartingIndex", testStartingIndex),
+        ("testElementIndex", testElementIndex),
     ]
+
 }
